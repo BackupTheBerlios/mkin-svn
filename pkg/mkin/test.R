5f36149e7932595b2d7fb42e238e3475b1a6e4df
ranke <ranke@3923ff91-21cf-406d-a444-b612330c3fac>
2010-05-20 16:40:19 +0000

    Add the example data from the KinGUI Piacenza paper with the model and the fit.

--- pkg/mkin/test.R
+++ pkg/mkin/test.R
@@ -68,14 +68,34 @@ d3$value <- cumsum(c(0, 6.3, 7.2, 2.0, 1.0, -2, -3.5, -4, -4.5))
 (observed <- rbind(d, d2, d3))
 
 FOMC <- mkinmod(parent = list(type="FOMC"))
-SFO_SFO <- mkinmod(parent = list(type="SFO", to = "m1"), m1 = list(type="SFO"))
+FOMC_SFO <- mkinmod(parent = list(type="FOMC", to = "m1"), m1 = list(type="SFO"))
 FOMC_SFO2 <- mkinmod(parent = list(type="FOMC", to = c("m1", "m2")),
   m1 = list(type = "SFO"), m2 = list(type="SFO"))
 
 summary(mkinfit(SFORB_SFO, observed, plot=TRUE)
 summary(mkinfit(SFO_SFO, observed, plot=TRUE))
 summary(mkinfit(FOMC, observed, plot=TRUE))
-fit <- mkinfit(FOMC_SFO, observed, parms.ini = c(1, 2, 0.001, 0.5), plot=TRUE)
-fit <- mkinfit(FOMC_SFO2, observed, parms.ini = c(1, 2, 0.001, 0.1, 0.5, 0.5), plot=TRUE)
+fit <- mkinfit(FOMC_SFO, observed, plot=TRUE)
+fit <- mkinfit(FOMC_SFO, observed, parms.ini = c(1, 10, 0.1, 0.5), plot=TRUE)
+fit <- mkinfit(FOMC_SFO2, observed, parms.ini = c(1, 10, 0.1, 0.1, 0.5, 0.5), plot = TRUE)
 summary(fit)
 
+schaefer07_complex_case <- data.frame(
+  time = c(0, 1, 3, 7, 14, 30, 62, 100),
+  parent = c(93.2, 89.4, 79.7, 61.1, 48.2, 15.9, 6.5, 6.0),
+  A1 = c(NA, NA, 0.55, 6.87, 17.08, 21.68, 15.77, 13.63),
+  B1 = c(NA, NA, NA, 0.55, 2.31, 15.76, 6.36, 3.74),
+  C1 = c(NA, 0.55, 3.20, 5.46, 12.55, 10.45, 4.74, 4.33),
+  A2 = c(NA, 0.55, 1.41, 0.55, 1.29, 1.95, 3.54, 3.86))
+
+data <- mkin_wide_to_long(schaefer07_complex_case, time = "time")
+
+schaefer07_complex_model <- mkinmod(
+  parent = list(type = "SFO", to = c("A1", "B1", "C1"), sink = FALSE),
+  A1 = list(type = "SFO", to = "A2"),
+  B1 = list(type = "SFO"),
+  C1 = list(type = "SFO"),
+  A2 = list(type = "SFO"))
+
+system.time(fit <- mkinfit(schaefer07_complex_model, data, plot=TRUE))
+summary(fit, data=FALSE)
