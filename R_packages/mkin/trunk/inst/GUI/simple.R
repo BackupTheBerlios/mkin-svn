2c9165c439b466ef7799def035464b56b158c08f
jranke <jranke@3923ff91-21cf-406d-a444-b612330c3fac>
2010-10-11 20:44:38 +0000

    Some little progress with the GUI experiment

--- R_packages/mkin/trunk/inst/GUI/simple.R
+++ R_packages/mkin/trunk/inst/GUI/simple.R
@@ -21,11 +21,40 @@ menulist <- list(Project = list(
 
 mb = gmenu(menulist, cont=w)
 
-# Layout
+# Project definition
+pr <- gexpandgroup("Project", cont=w)
+
+n.observed <- 1
+max.n.observed <- 20
+observed.names = c("parent", paste("M", 1:19, sep=""))
+
+prg <- ggroup(horizontal=FALSE, cont = pr)
+prl <- glayout(cont = prg)
+prl[1,1] <- glabel("Number of observed variables", cont=prl)
+prl[1,2] <- (n.observed.gw = gcombobox(
+  1:max.n.observed, 
+  handler = function(h, ...) {
+    n.observed <- svalue(n.observed.gw)
+  },
+  cont=prl))
+
+observed.gw <- gdf(
+  items = data.frame(Index = 1:max.n.observed, Name = observed.names, stringsAsFactors=FALSE),
+  name = "Names of observed variables",
+  width = 500, height=500, cont=prg)
+visible(observed.gw) <- FALSE
+
+visible(pr) <- TRUE
+
+# Dataset definition
 ds <- gexpandgroup("Datasets", cont=w)
 visible(ds) <- FALSE
+
+# Model definition
 ms <- gexpandgroup("Models", cont=w)
-visible(ds) <- FALSE
-mw <- gframe("Main window", cont=w)
+visible(ms) <- FALSE
+
+# Evaluation window
+mw <- gframe("Evaluations", cont=w)
 
 visible(w) <- TRUE 
