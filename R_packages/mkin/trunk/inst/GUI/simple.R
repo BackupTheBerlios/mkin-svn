d8463d300b8ee830090bbd35df07ac54761a62c2
jranke <jranke@3923ff91-21cf-406d-a444-b612330c3fac>
2010-10-18 13:00:05 +0000

    Reduced the filtering problem by just only allowing 9 variables

--- R_packages/mkin/trunk/inst/GUI/simple.R
+++ R_packages/mkin/trunk/inst/GUI/simple.R
@@ -25,8 +25,8 @@ mb = gmenu(menulist, cont=w)
 pr <- gexpandgroup("Project", cont=w)
 
 n.observed <- 1
-max.n.observed <- 20
-observed.names = c("parent", paste("M", 1:19, sep=""))
+max.n.observed <- 9
+observed.names = c("parent", paste("M", 1:(max.n.observed - 1), sep=""))
 
 prg <- ggroup(horizontal=FALSE, cont = pr)
 prl <- glayout(cont = prg)
@@ -35,9 +35,7 @@ prl[1,2] <- (n.observed.gw = gcombobox(
   1:max.n.observed, 
   handler = function(h, ...) {
     n.observed <- svalue(n.observed.gw)
-    filter.expression <- ifelse(n.observed < 10,
-      paste("^[1-", n.observed, "]$", sep=""),
-      paste("^[ 1][0-", n.observed, "]$", sep=""))
+    filter.expression <- paste("^[1-", n.observed, "]$", sep="")
     observed.gw$filter("Index", filter.expression)
   },
   cont=prl))
@@ -46,7 +44,7 @@ observed.gw <- gdf(
   items = data.frame(Index = 1:max.n.observed, 
     Name = observed.names, stringsAsFactors=FALSE),
   name = "Names of observed variables",
-  width = 500, height=500, cont=prg)
+  width = 500, height=250, cont=prg)
 
 visible(pr) <- TRUE
 
